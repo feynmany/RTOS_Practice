81e2de6320eadad58ea101e219341ba5a7dbe913
Kimi <qimin.yao@outlook.com>
2025-07-24 22:01:54 +0800

    update ignore

--- FreeRTOS_001Task/Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
+++
@@ -1,6 +0,0 @@
-ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/Dev/FreeRTOS/Workspace/RTOS_WS/FreeRTOS_001Task/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-C:/Dev/FreeRTOS/Workspace/RTOS_WS/FreeRTOS_001Task/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
